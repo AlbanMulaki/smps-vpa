ebd7f9ad351b4207c9c9e91223c3f8be279bf003
Alban Mulaki <alban.mulaki@gmail.com>
2016-06-03 01:14:17 +0200

    Ridizajnimi i login page #23

--- vpasis/app/views/login.blade.php
+++ vpasis/app/views/login.blade.php
@@ -2,66 +2,43 @@
 <html>
     <head>
         <title> VPA - Login</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap -->
-        
-    {{ HTML::style('style/css/bootstrap.min.css') }}
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        {{ HTML::style('style/css/bootstrap.min.css') }}
+        {{ HTML::style('style/css/AdminLTE.min.css') }}
     </head>
-    <body>
-
-
-        <div class="container well well-lg" >
-            <br>
-            {{ Form::open(array('url' => action('AuthController@postLogin'),'method'=>'POST','class'=>'form-horizontal')) }}
-            <fieldset >
-                <div class="col-sm-5"></div>
-                <div class="col-sm-4">
-                    <div class="col-sm-8" ><img src="{{asset("img/login_logo.png")}}" alt="VPA - Login" class="img-responsive"> </img>
-                    </div>
-                </div> 
-                <div class="col-sm-5"></div>
-
-            </fieldset>
-            {{ $errors->first('id') }}
-            {{ $errors->first('us') }}
-            {{ $errors->first('password') }}
-            <!-- if there are login errors, show them here -->
-            <br>
-            <br>
-
-
-
-            <div class="form-group ">
-                <div class="col-sm-5"></div>
-                <div class="col-sm-3">
-                    <div class="input-group">
-                        <span class="input-group-addon">{{ Lang::get('general.id') }}</span>
-                        {{ Form::text('id', Input::old('id'), array('placeholder' => Lang::get('general.id'),'class'=>'form-control input-lg  ')) }}
+    <body class="hold-transition lockscreen">
+        <div class="lockscreen-wrapper">
+            <div class="lockscreen-logo">
+                <a href="#"><b>SMPS</b> Login</a>
+            </div>
+            @if($errors->has())
+            <div class="callout callout-danger">
+                <h4>Gabim!</h4>
+                <p>{{ $errors->first('id') }}</p>
+                <p>{{ $errors->first('us') }}</p>
+                <p>{{ $errors->first('password') }}</p>
+            </div>
+            @endif
+            <div class="lockscreen-item">
+                <div class="lockscreen-image">
+                    <img src="{{asset("img/login_logo.png")}}" alt="VPA - Login" class="img-responsive">
+                </div>
+                {{ Form::open(array('url' => action('AuthController@postLogin'),'method'=>'POST','class'=>'lockscreen-credentials')) }}
+                <div class="input-group">
+                    {{ Form::text('id', Input::old('id'), array('placeholder' => Lang::get('general.id'),'class'=>'form-control input-lg  ')) }}
+                    {{ Form::password('passwd',array('placeholder' => Lang::get('general.password'),'class'=>'form-control input-lg ')) }}
+                    <div class="input-group-btn ">
+                        <button type="submit" class="btn"><i class="fa fa-arrow-right text-muted"></i></button>
                     </div>
                 </div>
+                {{ Form::close() }}
             </div>
-
-
-            <div class="form-group ">
-                <div class="col-sm-5"></div>
-                <div class="col-sm-3">
-                    <div class="input-group">
-                        <span class="input-group-addon">{{ Lang::get('general.password') }}</span>
-                        {{ Form::password('passwd',array('placeholder' => Lang::get('general.password'),'class'=>'form-control input-lg ')) }}
-                    </div>  </div>
-            </div>
-            <div class="form-group " >
-                <div class="col-sm-offset-7">
-                    {{ Form::submit(Lang::get('general.login') ,array('class'=>'btn  btn-primary  btn-lg ')) }}
-                </div>  
-            </div>
-
-            {{ Form::close() }}
-        </div>     
-
-
-    {{ HTML::script('style/js/jquery-1.11.1.min.js') }}
-    {{ HTML::script('style/js/bootstrap.min.js') }}
+           
+        </div>
+        {{ HTML::script('style/js/jquery-1.11.1.min.js') }}
+        {{ HTML::script('style/js/bootstrap.min.js') }}
     </body>
 
 </html>
